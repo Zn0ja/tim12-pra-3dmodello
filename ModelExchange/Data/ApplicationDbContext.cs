2bca9bc91e3290756040b087091c532f95614e08
Zn0ja <noa.putnik@gmail.com>
2026-01-31 14:43:26 +0100

    Add EF Core ApplicationDbContext

---
+++ ModelExchange/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ModelExchange.Models;
+
+namespace ModelExchange.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Model3D> Models3D => Set<Model3D>();
+        public DbSet<Favorite> Favorites => Set<Favorite>();
+
+
+    }
+}
