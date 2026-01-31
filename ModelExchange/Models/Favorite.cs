e1ca20b6b23ec9cbc0c74c77c7bbcf2bf4538c63
Marijan-V <mvelnic@algebra.hr>
2026-01-31 14:40:26 +0100

    Add the main models

---
+++ ModelExchange/Models/Favorite.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelExchange.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        public int Model3DId { get; set; }
+        public Model3D Model3D { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ ModelExchange/Models/Model3D.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelExchange.Models
+{
+    public class Model3D
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string OwnerUserId { get; set; } = default!;
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(2000)]
+        public string? Description { get; set; }
+
+        [MaxLength(400)]
+        public string? Tags { get; set; }
+
+        [MaxLength(80)]
+        public string? Category { get; set; }
+
+        [Required]
+        public string Visibility { get; set; } = "public";
+
+        [Required]
+        public string FilePath { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
